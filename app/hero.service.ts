2715e16b8a51b1ab6b88a987b69bd67ddfdd9cbb
Fernandohn <fernandohn@hotmail.com.br>
2016-07-12 21:58:27 -0300

    Chapter 5

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,12 +6,16 @@ import { Hero } from './hero';
 
 @Injectable()
 export class HeroService {
+  getHero(id: number) {
+    return this.getHeroes()
+             .then(heroes => heroes.find(hero => hero.id === id));
+  }
+
   getHeroes() {
     return Promise.resolve(HEROES);
   }
 
   getHeroesSlowly() {
     return new Promise<Hero[]>(resolve => setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-  );
-}
+  );}
 }
